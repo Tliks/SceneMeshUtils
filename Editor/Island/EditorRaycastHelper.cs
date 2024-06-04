6203ee0596dd8ddc43bfc80f3fc8942c7d7d75cd
tliks <aoft725@gmail.com>
2024-06-04 19:46:57 +0900

    fix raycast

--- Editor/Island/EditorRaycastHelper.cs
+++ Editor/Island/EditorRaycastHelper.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
@@ -19,11 +17,6 @@ public struct ExtendedRaycastHit
 
 public static class EditorRaycastHelper
 {
-    private static readonly MethodInfo intersectRayMeshMethod = typeof(HandleUtility).GetMethod("IntersectRayMesh",
-        BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
-
-    private static GameObject lastGameObjectUnderCursor;
-
     public static bool RaycastAgainstScene(out ExtendedRaycastHit extendedHit)
     {
         if (Event.current == null)
@@ -31,54 +24,19 @@ public static class EditorRaycastHelper
             extendedHit = new ExtendedRaycastHit();
             return false;
         }
-        Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
 
-        GameObject gameObjectUnderCursor;
         switch (Event.current.type)
         {
             case EventType.Layout:
             case EventType.Repaint:
             case EventType.ExecuteCommand:
-                gameObjectUnderCursor = lastGameObjectUnderCursor;
                 break;
             default:
-                gameObjectUnderCursor = HandleUtility.PickGameObject(Event.current.mousePosition, false);
+                HandleUtility.PickGameObject(Event.current.mousePosition, false);
                 break;
         }
-
-        if (gameObjectUnderCursor)
-        {
-            Mesh meshUnderCursor = null;
-            if (gameObjectUnderCursor.TryGetComponent(out MeshFilter meshFilter))
-                meshUnderCursor = meshFilter.sharedMesh;
-            if (!meshUnderCursor && 
-                gameObjectUnderCursor.TryGetComponent(out SkinnedMeshRenderer skinnedMeshRenderer))
-            {
-                Mesh bakedMesh = new Mesh();
-                skinnedMeshRenderer.BakeMesh(bakedMesh);
-                meshUnderCursor = bakedMesh;
-            }
-            
-            /*
-            if (meshUnderCursor)
-            {
-                lastGameObjectUnderCursor = gameObjectUnderCursor;
-
-                object[] rayMeshParameters = new object[]
-                    { ray, meshUnderCursor, gameObjectUnderCursor.transform.localToWorldMatrix, null };
-                if ((bool) intersectRayMeshMethod.Invoke(null, rayMeshParameters))
-                {
-                    RaycastHit tempHit = (RaycastHit) rayMeshParameters[3];
-                    extendedHit = new ExtendedRaycastHit(tempHit, gameObjectUnderCursor.transform, tempHit.triangleIndex);
-                    Debug.Log("bb");
-                    return true;
-                }
-            }
-            else
-                lastGameObjectUnderCursor = null;
-            */
-        }
-
+        
+        Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
         object raycastResult = HandleUtility.RaySnap(ray);
         if (raycastResult != null && raycastResult is RaycastHit tempHit2)
         {
