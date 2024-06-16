d28d724a44eb6490aed963f37abfb545caa50610
tliks <aoft725@gmail.com>
2024-06-16 22:24:39 +0900

    add locale of advanced and helpbox

--- Editor/Localization.cs
+++ Editor/Localization.cs
@@ -8,6 +8,7 @@ public class LocalizationEditor
     
     private static Dictionary<string, string[]> _LocalizedText = new Dictionary<string, string[]>
     {
+        { "advancedoptions", new string[] {"Advanced Options", "高度なオプション"}},
         { "UndoButton", new string[] { "Undo (Ctrl+Z)", "元に戻す (Ctrl+Z)" } },
         { "RedoButton", new string[] { "Redo (Ctrl+Y)", "やり直し (Ctrl+Y)" } },
         { "PreviewModeLabel", new string[] { "Preview Mode: ", "プレビューモード: " } },
@@ -37,7 +38,9 @@ public class LocalizationEditor
         { "tooltip.AdditionalTransformsToggle", new string[] { "Output Additional PhysBones Affected Transforms for exact PhysBone movement", "正確なPhysBoneの動きのために追加のPhysBones Affected Transformsを出力" } },
         { "tooltip.IncludeIgnoreTransformsToggle", new string[] { "Output PhysBone's IgnoreTransforms", "PhysBoneのIgnoreTransformsを出力する" } },
         { "tooltip.RenameRootTransformToggle", new string[] { "Not Recommended: Due to the specifications of modular avatar, costume-side physbones may be deleted in some cases, so renaming physbone RootTransform will ensure that the costume-side physbones are integrated. This may cause duplication.", "推奨されません。モジュラーアバターの仕様により、場合によっては衣装側の物理ボーンが削除されることがあります。そのため、PhysBoneのRootTransformの名前を変更することで、衣装側の物理ボーンが統合されることを保証します。これにより重複が発生する可能性があります。" } },
-        { "tooltip.SpecifyRootObjectLabel", new string[] { "The default root object is the parent object of the specified skinned mesh renderer object", "デフォルトのルートオブジェクトは、指定されたSkinned Mesh Rendererオブジェクトの親オブジェクトです" } }
+        { "tooltip.SpecifyRootObjectLabel", new string[] { "The default root object is the parent object of the specified skinned mesh renderer object", "デフォルトのルートオブジェクトは、指定されたSkinned Mesh Rendererオブジェクトの親オブジェクトです" } },
+        { "tooltip.SplitMeshMoreToggle", new string[] {"Splits the mesh into smaller pieces. This is off by default as it can create unnecessarily small separations.", "メッシュをさらに細かく分離します。無意味に小さな分離をすることがあるのでデフォルトではオフになっています。"}},
+        { "tooltip.SelectAllInRangeToggle", new string[] {"Range selection options. By default, only islands that are completely within the dragged range are selected. Change this so that islands are selected even if only a part of them is within the range.", "範囲選択に関するオプションです。デフォルトでは、ドラッグされた範囲内に完全に含まれているアイランドのみが選択されます。これを、アイランドの一部でも範囲内にある場合に選択されるように変更します。"}}
     };
     
     private const string PreferenceKey = "com.aoyon.module-creator.lang";
