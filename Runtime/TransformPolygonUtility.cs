15015e8d7742e612c0729623b17161bded78cab2
tliks <aoft725@gmail.com>
2024-07-23 00:15:01 +0900

    chore: user HideInInspector to some values

--- Runtime/TransformPolygonUtility.cs
+++ Runtime/TransformPolygonUtility.cs
@@ -5,14 +5,19 @@ namespace com.aoyon.modulecreator.runtime
 {
     public class TransformPolygonUtility : MonoBehaviour
     {
+        [HideInInspector]
         public SkinnedMeshRenderer origSkinnedMeshRenderer;
+        [HideInInspector]
         public string rootname;
+        [HideInInspector]
         public Mesh originalMesh;
+        [HideInInspector]
         public HashSet<int> triangleIndices;
 
         public Vector3 position = Vector3.zero;
         public Vector3 rotation = Vector3.zero;
         public Vector3 scale = Vector3.one;
+        [HideInInspector]
         public Vector3 centroid = Vector3.zero;
         
         public void Initialize(SkinnedMeshRenderer origSkinnedMeshRenderer, string rootname, Mesh originalMesh, HashSet<int> triangleIndices)
