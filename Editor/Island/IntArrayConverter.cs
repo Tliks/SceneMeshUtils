4bbc9a96d1814360cd39ee22d874ec9d63ee3d31
Tliks <aoft725@gmail.com>
2024-08-14 14:13:24 +0900

    draft

--- Editor/Island/IntArrayConverter.cs
+++ Editor/Island/IntArrayConverter.cs
@@ -4,48 +4,46 @@ using UnityEngine;
 
 namespace com.aoyon.modulecreator
 {
-    public static class IntArrayConverter
+    public static class TriangleConverter
     {
-        public static Vector3[] Decode(Mesh mesh, int[] triangleIndices)
+        public static Vector3[] Encode(Mesh mesh, int[] triangleIndices)
         {
             Vector3[] vertices = mesh.vertices;
-            Vector3[] positions = new Vector3[triangleIndices.Length * 3]; // triangleIndicesは三角形のインデックスなので、頂点数は3倍
+            int[] triagnles = mesh.triangles;
+            
+            HashSet<Vector3> positions = new HashSet<Vector3>();
 
             for (int i = 0; i < triangleIndices.Length; i++)
             {
                 int triangleIndex = triangleIndices[i];
-                positions[i * 3] = vertices[mesh.triangles[triangleIndex * 3]];
-                positions[i * 3 + 1] = vertices[mesh.triangles[triangleIndex * 3 + 1]];
-                positions[i * 3 + 2] = vertices[mesh.triangles[triangleIndex * 3 + 2]];
+                positions.Add(vertices[triagnles[triangleIndex * 3]]);
+                positions.Add(vertices[triagnles[triangleIndex * 3 + 1]]);
+                positions.Add(vertices[triagnles[triangleIndex * 3 + 2]]);
             }
 
-            return positions;
+            return positions.ToArray();
         }
 
-        public static int[] Encode(Mesh mesh, Vector3[] positions)
-        {
+        public static int[] Decode(Mesh mesh, Vector3[] positions)
+        {   
+            Vector3[] vertices = mesh.vertices;
             int[] triangles = mesh.triangles;
+            HashSet<Vector3> positionSet = new HashSet<Vector3>(positions); 
+
             List<int> triangleIndices = new List<int>();
 
-            for (int i = 0; i < positions.Length; i += 3) // positionsは3つずつが1つの三角形を表す
+            for (int j = 0; j < triangles.Length; j += 3)
             {
-                Vector3 p0 = positions[i];
-                Vector3 p1 = positions[i + 1];
-                Vector3 p2 = positions[i + 2];
-
-                for (int j = 0; j < triangles.Length; j += 3)
+                if (positionSet.Contains(vertices[triangles[j]]) &&
+                    positionSet.Contains(vertices[triangles[j + 1]]) &&
+                    positionSet.Contains(vertices[triangles[j + 2]]))
                 {
-                    if (mesh.vertices[triangles[j]] == p0 &&
-                        mesh.vertices[triangles[j + 1]] == p1 &&
-                        mesh.vertices[triangles[j + 2]] == p2)
-                    {
-                        triangleIndices.Add(j / 3); // jは頂点インデックスなので、3で割って三角形インデックスに変換
-                        break;
-                    }
+                    triangleIndices.Add(j / 3);
                 }
             }
 
             return triangleIndices.ToArray();
         }
-        }
+
+    }
 }
